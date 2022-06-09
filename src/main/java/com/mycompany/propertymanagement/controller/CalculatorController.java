b00b94c8f85e6e8f19112b35e8ef7e21c0f50efa
Saumyashree <Kumarisaumyashree@gmail.com>
2022-06-09 18:58:49 +0530

    calculatorTest Unit tasting complted with code covrage

--- src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
@@ -33,7 +33,7 @@ public class CalculatorController {
     public ResponseEntity<Double> multiply(@RequestBody CalculatorDTO calculatorDTO){
         Double result = null;
         result = calculatorDTO.getNum1() * calculatorDTO.getNum2() * calculatorDTO.getNum3() * calculatorDTO.getNum4();
-        ResponseEntity<Double> responseEntity = new ResponseEntity<Double>(result, HttpStatus.CREATED);
+        ResponseEntity<Double> responseEntity = new ResponseEntity<Double>(result, HttpStatus.OK);
         return responseEntity;
     }
 }
--- src/test/java/com/mycompany/propertymanagement/controller/CalculatorControllerTest.java
+++ src/test/java/com/mycompany/propertymanagement/controller/CalculatorControllerTest.java
@@ -1,18 +1,24 @@
 package com.mycompany.propertymanagement.controller;
 
+import com.mycompany.propertymanagement.dto.CalculatorDTO;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("test addtion scenario sucess")
 @ExtendWith(MockitoExtension.class)
 public class CalculatorControllerTest {
-    @InjectMocks
-    private CalculatorController calculatorController;
+    @InjectMocks      //to give it to memory other wise we get null pointer exception
+    private CalculatorController calculatorController;  //WE ARE CALLING calculator controll
+
     static Double num1;
     static Double num2;
     static Double num3;
-
     @BeforeAll
     static void beforeAll() {
         System.out.println("BeforeAll");
@@ -53,7 +59,7 @@ public class CalculatorControllerTest {
     }
 
     @Test
-   // @Disabled
+    // @Disabled
     @DisplayName("Test Addition failure senario")
     void testAddFunction_Failure() {
         Double result = calculatorController.add(3.5, 3.5, 3.5);
@@ -66,15 +72,28 @@ public class CalculatorControllerTest {
     @Test
     @DisplayName("Test sub success num1>num2 senario")
     public void testSubFunction_num1_It_num2() {
-        Double result = calculatorController.substract(num1 , num2+1);
+        Double result = calculatorController.substract(num1, num2 + 1);
         Assertions.assertEquals(1.0, result);
     }
 
     @Test
     @DisplayName("Test sub success num1>num2 senario")
     public void testSubFunction_num1_gt_num2() {
-        Double result = calculatorController.substract(num1+1, num2 );
+        Double result = calculatorController.substract(num1 + 1, num2);
         Assertions.assertEquals(1.0, result);
     }
 
+    @DisplayName("Test multiplication")
+    void testMultiply() {
+        CalculatorDTO calculatorDTO = new CalculatorDTO();
+        calculatorDTO.setNum1(num1);
+        calculatorDTO.setNum2(num2);
+        calculatorDTO.setNum3(num3);
+        calculatorDTO.setNum4(2.0);
+
+        ResponseEntity<Double> responseEntity = calculatorController.multiply(calculatorDTO);
+        assertEquals(1328.25, responseEntity.getBody());
+        assertEquals(HttpStatus.OK.value(), responseEntity.getStatusCodeValue(), "expecting the status as OK");
+
+    }
 }
